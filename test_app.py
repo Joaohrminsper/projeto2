4a17547f1692178129797e31155647f8d55199ac
Vicente <vicentevn@al.insper.edu.br>
2026-09-08 09:06:25 -0300

    Add pytest-dependency to requirements and enhance tests for property validation

--- test_app.py
+++ test_app.py
@@ -1,7 +1,33 @@
+import pytest
+
+
+@pytest.mark.dependency()
 def test_listar_imoveis_retorna_200(client):
     response = client.get("/imoveis")
     assert response.status_code == 200
 
-def test_listar_imoveis_retorna_lista_vazia(client):
+
+@pytest.mark.dependency(depends=["test_listar_imoveis_retorna_200"])
+def test_listar_imoveis_retorna_lista_nao_vazia(client):
     response = client.get("/imoveis")
-    assert response.get_json() == []
+    assert len(response.get_json()) > 0
+
+
+@pytest.mark.dependency(depends=["test_listar_imoveis_retorna_200"])
+def test_listar_imoveis_retorna_dicionarios_com_chaves_esperadas(client):
+    response = client.get("/imoveis")
+    imoveis = response.get_json()
+    chaves_esperadas = {
+        "bairro",
+        "cep",
+        "cidade",
+        "data_aquisicao",
+        "id",
+        "logradouro",
+        "tipo",
+        "tipo_logradouro",
+        "valor",
+    }
+
+    assert len(imoveis) > 0
+    assert all(set(imovel.keys()) == chaves_esperadas for imovel in imoveis)
