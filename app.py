6d6aa04cb1d734ec6d732785a50463a69ab0104d
Vicente <vicentevn@al.insper.edu.br>
2026-09-08 08:51:10 -0300

    Primeira rota imóveis e implementação da conexão com o banco de dados.

--- app.py
+++ app.py
@@ -1,11 +1,30 @@
 from flask import Flask, jsonify
+import mysql.connector
 
 app = Flask(__name__)
 
 
+def conectar():
+    return mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="241207",
+        database="imobiliaria"
+    )
+
+
 @app.route("/imoveis", methods=["GET"])
 def listar_imoveis():
-    return jsonify([])
+    conexao = conectar()
+    cursor = conexao.cursor(dictionary=True)
+
+    cursor.execute("SELECT * FROM imoveis")
+    imoveis = cursor.fetchall()
+
+    cursor.close()
+    conexao.close()
+
+    return jsonify(imoveis)
 
 
 if __name__ == "__main__":
