1f9b6f9e2e0038b4eab9b32236adb85b20ffe092
Vicente <vicentevn@al.insper.edu.br>
2026-09-08 08:01:34 -0300

    Refactor app.py to remove unused routes and add requirements.txt for dependencies

--- app.py
+++ app.py
@@ -1,37 +1,12 @@
-from flask import Flask, request, jsonify
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route('/test', methods=["GET"])
-def hello_world_get():
-    return jsonify({"message": "Tudo certo!"}), 200
 
-@app.route('/test', methods=["DELETE"])
-def hello_world_delete():
-    req_data = request.get_json()
+@app.route("/imoveis", methods=["GET"])
+def listar_imoveis():
+    return jsonify([])
 
-    if ("name" not in req_data):
-        return jsonify({"message": "Erro!"}), 400
 
-    return jsonify({"message": f"Olá {req_data['name']}!"}), 200
-
-@app.route('/test', methods=["PUT"])
-def hello_world_put():
-    req_data = request.get_json()
-
-    if ("name" not in req_data):
-        return jsonify({"message": "Erro!"}), 400
-
-    return jsonify({"message": f"Olá {req_data['name']}!"}), 200
-
-@app.route('/test', methods=["POST"])
-def hello_world_post():
-    req_data = request.get_json()
-
-    if ("name" not in req_data):
-        return jsonify({"message": "Erro!"}), 400
-
-    return jsonify({"message": f"Olá {req_data['name']}!"}), 200
-
-if __name__ == '__main__':
-    app.run(debug=True, port=5500)
+if __name__ == "__main__":
+    app.run(debug=True)
